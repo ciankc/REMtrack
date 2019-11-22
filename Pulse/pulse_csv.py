2e9b9c46a961867f104a4f1c7900fe46976835fd
ciankc <ciancost@Cians-MacBook-Pro.local>
2019-11-21 23:32:05 -0800

    finished pulse

---
+++ Pulse/pulse_csv.py
@@ -0,0 +1,17 @@
+import serial
+import time
+import datetime
+
+ser = serial.Serial(port='/dev/tty.usbmodem1421', baudrate=9600)
+
+print("connected to: " + ser.portstr)
+
+while True:
+    line = str(ser.readline())
+    val = line[2:-5]
+    #timestamp = str(time.time())
+    timestamp = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+    with open('output_11_17.csv', 'a') as pyfile:
+        pyfile.write(timestamp + ',' + val + '\n')
+
+ser.close()
